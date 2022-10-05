e3cc5fc9bedfe4f40bb25371e14c81ed5698d0dc
silviordjr <silviordjr@outlook.com>
2022-10-05 09:35:20 -0300

    refactoring header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,52 +1,23 @@
-import Link from 'next/link'
-import { useEffect } from 'react'
-import { useState } from 'react';
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
+
+const navigationRoutes = ["home", "about"];
 
 export default function Header () {
+  const router = useRouter();
 
-  const changeFlag = (button: string) => {
-    if (button === 'home'){
-      const menu = document.querySelector("#home-menu")
-      const desactive = document.querySelector("#about-menu")
+  const navigation = navigationRoutes.map((singleRoute) => {
+    return (
+      <NavigationLink
+        key={singleRoute}
+        href={`/${singleRoute}`}
+        text={singleRoute === "home" ? "Home" : "About Us"}
+        router={router}
+      />
+    )
+  })
 
-      desactive?.classList.remove('text-red-700')
-      menu?.classList.add('text-red-700')
-    }
-
-    if (button === 'about'){
-      const menu = document.querySelector("#about-menu")
-      const desactive = document.querySelector("#home-menu")
-
-      desactive?.classList.remove('text-red-700')
-      menu?.classList.add('text-red-700')
-    }
-  }
-
-  useEffect(() => {
-    const home = document.querySelector("#home-page")
-    const about = document.querySelector("#about-page")
-
-    if (home) {
-      const menu = document.querySelector("#home-menu")
-      const desactive = document.querySelector("#about-menu")
-
-      desactive?.classList.remove('text-red-700')
-      menu?.classList.add('text-red-700')
-    } else if (about){
-      const menu = document.querySelector("#about-menu")
-      const desactive = document.querySelector("#home-menu")
-
-      desactive?.classList.remove('text-red-700')
-      menu?.classList.add('text-red-700')
-    } else {
-      const menuDesactive = document.querySelector("#home-menu")
-      const aboutDesactive = document.querySelector("#about-menu")
-
-      menuDesactive?.classList.remove('text-red-700')
-      aboutDesactive?.classList.remove('text-red-700')
-    }
-  }, [])
-    return(
+  return (
     <>
     <header className="w-full bg-gray-300 shadow-xl h-16">
       <div className='flex justify-between items-center max-w-screen-md mx-auto h-16 px-4 lg:px-0'>
@@ -54,14 +25,26 @@ export default function Header () {
         <h2 className='font-sans text-xl font-semibold'>Lorem Ipsum Blog</h2>
       </div>
 
-      <nav>
-        <ul className="flex">
-          <li className='pl-4' id='home-menu' onClick={() => changeFlag('home')}><Link href="/"><a>Home</a></Link></li>
-          <li className='pl-4' id='about-menu' onClick={() => changeFlag('about')}><Link href="/about"><a>About Us</a></Link></li>
-        </ul>
+      <nav className="nav-container">
+        {navigation}
       </nav>
       </div>
     </header>
     </>
-    )
+  )
+}
+
+function NavigationLink (props: any) {
+  const {router, href, text} = props
+  const isActive = router.asPath === (href === "/home" ? "/" : href);
+
+  return (
+    <Link href={href === "/home" ? "/" : href} passHref>
+      <a 
+        href={href === "/home" ? "/" : href}
+        className={`${isActive && "nav-item-active"} nav-item ml-4`}>
+          {text}
+        </a>
+    </Link>
+  )
 }
